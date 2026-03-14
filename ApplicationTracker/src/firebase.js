a9c335db283ea0d2339207181d3629b8989eb086
sai sirisha <saisirisha6803@gmail.com>
2026-03-15 01:43:14 +0530

    Updated API KEY

--- ApplicationTracker/src/firebase.js
+++ ApplicationTracker/src/firebase.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB38JAmoFoQ7-1n5PXcbmn7um9raTo7tzs",
+  apiKey: apiKey,
   authDomain: "job-tracker-b8bc2.firebaseapp.com",
   projectId: "job-tracker-b8bc2",
   storageBucket: "job-tracker-b8bc2.firebasestorage.app",
